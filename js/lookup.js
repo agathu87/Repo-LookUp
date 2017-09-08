17cc9e7b99dc21b46b3f750ee73dd7ed889e56f6
Angela Gathu <agathu87@gmail.com>
2017-09-08 18:03:59 +0300

    update lookup.js file

---
+++ js/lookup.js
@@ -0,0 +1,23 @@
+var apiKey = require('./../.env').apiKey;
+
+lookUpRepos = function() {};
+
+lookUpRepos.prototype.getlookUp = function(userName, displayRepos) {
+  var i;
+  $('#displayRepos').empty();
+  $('#reposHeader').hide();
+  $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response) {
+    console.log(response);
+    for (i = 0; i < response.length; i++) {
+      if (response[i].description===null){
+        response[i].description = 'No description in repo';
+      }
+      $('#reposHeader').show();
+      displayRepos(response[i].name,response[i].description,);
+    }
+  }).fail(function(error) {
+    $('#display').text(error.response.message);
+  });
+};
+
+exports.lookUpReposModule = lookUpRepos;
